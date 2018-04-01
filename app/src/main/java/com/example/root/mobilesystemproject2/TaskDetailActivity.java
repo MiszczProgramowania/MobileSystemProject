a36ca22bd778602e96ca2dbc9a1b13c84048c436
MiszczProgramowania <budzyn.grzegorz.it@gmail.com>
2018-04-01 14:31:40 +0200

    date picker implemented

--- app/src/main/java/com/example/root/mobilesystemproject2/TaskDetailActivity.java
+++ app/src/main/java/com/example/root/mobilesystemproject2/TaskDetailActivity.java
@@ -1,10 +1,16 @@
 package com.example.root.mobilesystemproject2;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class TaskDetailActivity extends AppCompatActivity {
 
@@ -12,6 +18,36 @@ public class TaskDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail);
+        registerDatePicker();
+    }
+
+    private void registerDatePicker() {
+        final TaskDetailActivity self = this;
+        getEndDate().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR);
+                int mMonth = c.get(Calendar.MONTH);
+                int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog =
+                        new DatePickerDialog(
+                                self,
+                                new DatePickerDialog.OnDateSetListener() {
+                                    @Override
+                                    public void onDateSet(DatePicker datePicker, int yeat, int month, int day) {
+                                        Date d = new Date(yeat, month, day);
+                                        getEndDate().setText(d.toString());
+                                    }
+                                },
+                                mYear,
+                                mMonth,
+                                mDay
+                        );
+                dialog.show();
+            }
+        });
     }
 
     private EditText getName() {
